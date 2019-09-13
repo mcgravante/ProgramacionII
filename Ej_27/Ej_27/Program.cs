5cc0402bedf324bfd6b01d06683fbe81952d27b1
matigravante <matiasgravante@hotmail.com>
2019-09-12 22:09:52 -0300

    Commit ej28

---
+++ Ej_27/Ej_27/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Ej_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numerosRandom = new List<int>();
+            Random numeroRandom = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                int aux = numeroRandom.Next(-1000000, 1000000);
+                if (aux != 0)
+                {
+                    numerosRandom.Add(aux);
+                }
+            }
+            foreach (int numero in numerosRandom)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------");
+
+
+            List<int> numerosPositivos = new List<int>();
+            foreach (int numero in numerosRandom)
+            {
+                if (numero > 0)
+                {
+                    numerosPositivos.Add(numero);
+                }
+            }
+            numerosPositivos.Sort();
+            numerosPositivos.Reverse();
+
+
+            foreach (int numero in numerosPositivos)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------");
+
+            List<int> numerosNegativos = new List<int>();
+
+            foreach (int numero in numerosRandom)
+            {
+                if (numero < 0)
+                {
+                    numerosNegativos.Add(numero);
+                }
+            }
+            numerosNegativos.Sort();
+
+            foreach (int numero in numerosNegativos)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+
+
+
+            Queue queue = new Queue();
+            Random numeroRandom2 = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                int aux = numeroRandom2.Next(-1000000, 1000000);
+                if (aux != 0)
+                {
+                    queue.Enqueue(aux);
+                }
+            }
+            foreach (int numero in queue)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------");
+
+
+            Queue queuePos = new Queue();
+            List<int> listaParaQueue = new List<int>();
+            foreach (int numero in queue)
+            {
+                if (numero > 0)
+                {
+                    listaParaQueue.Add(numero);
+                }
+            }
+            listaParaQueue.Sort();
+            listaParaQueue.Reverse();
+            foreach (int numParaQueue in listaParaQueue)
+            {
+                queuePos.Enqueue(numParaQueue);
+            }
+
+
+            foreach (int numero in queuePos)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------");
+
+            Queue queueNeg = new Queue();
+
+            List<int> listaParaQueue2 = new List<int>();
+            foreach (int numero in queue)
+            {
+                if (numero < 0)
+                {
+                    listaParaQueue2.Add(numero);
+                }
+            }
+            listaParaQueue2.Sort();
+            foreach (int numParaQueue in listaParaQueue2)
+            {
+                queueNeg.Enqueue(numParaQueue);
+            }
+
+
+            foreach (int numero in queueNeg)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------");
+
+            Stack stack = new Stack();
+            Random numeroRandom3 = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                int aux = numeroRandom3.Next(-1000000, 1000000);
+                if (aux != 0)
+                {
+                    stack.Push(aux);
+                }
+            }
+            foreach (int numero in stack)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------");
+
+
+            Stack stackPos = new Stack();
+            List<int> listaParaStackPos = new List<int>();
+            foreach (int numero in stack)
+            {
+                if (numero > 0)
+                {
+                    listaParaStackPos.Add(numero);
+                }
+            }
+            listaParaStackPos.Sort();
+            foreach (int numParaQueue in listaParaStackPos)
+            {
+                stackPos.Push(numParaQueue);
+            }
+
+
+            foreach (int numero in stackPos)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------");
+
+            Stack stackNeg = new Stack();
+
+            List<int> listaParaStackNeg = new List<int>();
+            foreach (int numero in stack)
+            {
+                if (numero < 0)
+                {
+                    listaParaStackNeg.Add(numero);
+                }
+            }
+            listaParaStackNeg.Sort();
+            listaParaStackNeg.Reverse();
+
+            foreach (int numParaQueue in listaParaStackNeg)
+            {
+                stackNeg.Push(numParaQueue);
+            }
+
+
+            foreach (int numero in stackNeg)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
---
+++ Ej_28/Ej_28/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej_28
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (KeyValuePair<string, int> kvp in dict)
+            {
+                MessageBox.Show(string.Format("Key = {0}, Value ={1}", kvp.Key, kvp.Value));
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Quito los espacios en blanco a izquierda y derecha
+            string texto = richTextBox1.Text.Trim();
+            // Divido la cadena a través de los espacios vacios ' ' y la recorro
+            foreach (string s in texto.Split(' '))
+            {
+                // Si la palabra no es null o vacia, incremento mi acumulador
+                if (!string.IsNullOrEmpty(s))
+                {
+                    if (dict.ContainsKey(s))
+                    {
+                        dict[s]++;
+                    }
+                    else
+                    {
+                        dict.Add(s, 1);
+                    }
+                }
+            }
+        }
+
+    }
+}
